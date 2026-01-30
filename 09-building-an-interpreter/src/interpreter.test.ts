5f55e250bed88f40ca0c7a3e728567222df1192a
richardcarlson3 <richardcarlson517@gmail.com>
2026-01-30 10:45:06 -0500

    Initial commit

--- 09-building-an-interpreter/src/interpreter.test.ts
+++ 09-building-an-interpreter/src/interpreter.test.ts
@@ -1,21 +1,204 @@
-import assert from "assert";
+import _assert from "assert";
 import { parseExpression, parseProgram } from "../include/parser.js";
 import { State, interpExpression, interpProgram, interpStatement } from "./interpreter.js";
 
 type RuntimeValue = number | boolean;
 
-function expectStateToBe(program: string, state: {[key: string]: RuntimeValue}) {
+function expectStateToBe(program: string, state: { [key: string]: RuntimeValue }) {
   expect(interpProgram(parseProgram(program)).asObject()).toEqual(state);
 }
 
 describe("interpExpression", () => {
   // Tests for interpExpression go here
+  it("evaluates multiplication with a variable", () => {
+    // create State with no parent State (global State)
+    const state = new State();
+    state.declare("x", 10);
+    const r = interpExpression(state, parseExpression("x * 2"));
+
+    expect(r).toBe(20);
+  });
+  it("evaluates and works with addition, subtraction, and division", () => {
+    const state = new State();
+    state.declare("x", 67);
+    state.declare("y", 420);
+    state.declare("z", 24);
+    const r1 = interpExpression(state, parseExpression("x + y"));
+    const r2 = interpExpression(state, parseExpression("x - y"));
+    const r3 = interpExpression(state, parseExpression("z / 4"));
+    expect(r1).toBe(487);
+    expect(r2).toBe(-353);
+    expect(r3).toBe(6);
+  });
+  it("evaluates hard expressions", () => {
+    const state = new State();
+    state.declare("x", 16);
+    state.declare("y", 2);
+    const z = interpExpression(state, parseExpression("x * (y + 8) - 20 / (1 + 3)"));
+    expect(z).toBe(155);
+  });
+  it("properly evaluates boolean expressions", () => {
+    const state = new State();
+    state.declare("x", 10);
+    state.declare("y", 21);
+    const z1 = interpExpression(state, parseExpression("x < y"));
+    const z2 = interpExpression(state, parseExpression("x > y"));
+    const z3 = interpExpression(state, parseExpression("x === 10"));
+    expect(z1).toBe(true);
+    expect(z2).toBe(false);
+    expect(z3).toBe(true);
+  });
+  it("evaluates nested boolean expressions", () => {
+    const state = new State();
+    state.declare("a", 1);
+    state.declare("b", 2);
+    state.declare("c", 3);
+    const r = interpExpression(state, parseExpression("(a < b) === (b < c)"));
+    expect(r).toBe(true);
+  });
+  it("evaluates && and || operators", () => {
+    const state = new State();
+    state.declare("x", 21);
+    state.declare("y", 67);
+    const r1 = interpExpression(state, parseExpression("(x < y) && (y === 67)"));
+    const r2 = interpExpression(state, parseExpression("(x > y) || (y === 1738)"));
+    const r3 = interpExpression(state, parseExpression("(y === 21) || (x > y)"));
+
+    expect(r3).toBe(false);
+    expect(r1).toBe(true);
+    expect(r2).toBe(false);
+  });
+  it("throws error for division by zero", () => {
+    const state = new State();
+    state.declare("x", 10);
+    expect(() => {
+      interpExpression(state, parseExpression("x / 0"));
+    }).toThrow("can't divide by zero");
+  });
+  it("throws error for a undefined variable", () => {
+    const state = new State();
+    expect(() => {
+      interpExpression(state, parseExpression("y + 67"));
+    }).toThrow("variable y not declared.");
+  });
+  it("throws error for type mismatches", () => {
+    const state = new State();
+    state.declare("x", 21);
+    state.declare("y", true);
+    expect(() => {
+      interpExpression(state, parseExpression("x + y"));
+    }).toThrow("+ requires number operands");
+    expect(() => {
+      interpExpression(state, parseExpression("x && y"));
+    }).toThrow("&& requires boolean operands");
+  });
 });
 
 describe("interpStatement", () => {
   // Tests for interpStatement go here.
+  it("throws error if assigning undeclared variable", () => {
+    const state = new State();
+    expect(() => {
+      interpStatement(state, parseProgram("y=5;")[0]);
+    }).toThrow("variable y not declared.");
+  });
+  it("throws error for duplicate variable declaration", () => {
+    const state = new State();
+    interpStatement(state, parseProgram("let x=5;")[0]);
+
+    expect(() => interpStatement(state, parseProgram("let x=10;")[0])).toThrow("variable x already declared.");
+  });
+
+  it("evaluates if-else with true condition (scoped)", () => {
+    const state = new State();
+    interpStatement(state, parseProgram("if (true) { let x=1; } else { let x=0; }")[0]);
+
+    expect(state.asObject()).toEqual({});
+  });
+
+  it("throws error if if-test is not boolean", () => {
+    const state = new State();
+
+    expect(() => interpStatement(state, parseProgram("if (1) {print(1);} else {print(2);}")[0])).toThrow(
+      "If condition must be a boolean"
+    );
+  });
+
+  it("executes print statement", () => {
+    const state = new State();
+    state.declare("x", 1);
+
+    const logSpy = jest.spyOn(console, "log");
+
+    interpStatement(state, parseProgram("print(x);")[0]);
+
+    expect(logSpy.mock.calls).toEqual([[1]]);
+    logSpy.mockRestore();
+  });
+
+  it("throws error for invalid variable in print", () => {
+    const state = new State();
+
+    expect(() => interpStatement(state, parseProgram("print(x);")[0])).toThrow("variable x not declared.");
+  });
+
+  it("else branch updates outer variable", () => {
+    const state = new State();
+    state.declare("x", 1);
+
+    interpStatement(state, parseProgram("if (false) {} else { x=99; }")[0]);
+
+    expect(state.asObject()).toEqual({ x: 99 });
+  });
 });
 
 describe("interpProgram", () => {
   // Tests for interpProgram go here
+
+  it("handles declarations and reassignment", () => {
+    const state = interpProgram(parseProgram(`let x = 67; x = 21;`));
+    expect(state.get("x")).toBe(21);
+  });
+  it("handles arithmetic and variable usage", () => {
+    const state = `let a = 4; let b = a + 5; let c=b * 3;`;
+    expectStateToBe(state, { a: 4, b: 9, c: 27 });
+  });
+
+  it("handles a while loop", () => {
+    const state = `let x = 0; while (x < 3) {x = x + 1;}`;
+    expectStateToBe(state, { x: 3 });
+  });
+
+  it("executes a block with multiple statements", () => {
+    const state = `
+    let x = 67;    
+    if (true) {
+        x = x + 2;
+        x = x * 2;
+      } else {
+      }`;
+    expectStateToBe(state, { x: 138 });
+  });
+
+  it("inner block does not effevt x", () => {
+    const state = `
+      let x = 67;
+      if (true) {
+        let x = 2;
+      } else {
+      }`;
+    expectStateToBe(state, { x: 67 });
+  });
+
+  it("inner loop changes y not x", () => {
+    const state = `
+      let x = 1;
+      let y = 21;
+      if (true) {
+        let x = 2;
+        y = x;
+      } else {
+      }`;
+    expectStateToBe(state, { x: 1, y: 2 });
+  });
 });
--- 09-building-an-interpreter/src/interpreter.ts
+++ 09-building-an-interpreter/src/interpreter.ts
@@ -5,26 +5,146 @@ type RuntimeValue = number | boolean;
 export function interpProgram(program: Statement[]): State {
   const globalScope = new State();
   // Add your implementation
+  for (const statement of program) {
+    interpStatement(globalScope, statement);
+  }
+
   return globalScope;
 }
 
 export function interpStatement(state: State, stmt: Statement): void {
+  switch (stmt.kind) {
+    case "let":
+      const value = interpExpression(state, stmt.expression);
+      state.declare(stmt.name, value);
+      return;
+    case "assignment":
+      const assignedVal = interpExpression(state, stmt.expression);
+      state.set(stmt.name, assignedVal);
+      return;
+    case "print":
+      const printVal = interpExpression(state, stmt.expression);
+      console.log(printVal);
+      break;
+    case "if":
+      interpIfStatement(state, stmt);
+      return;
+    case "while":
+      interpWhileStatement(state, stmt);
+      return;
+    default:
+      throw new Error("Unknown statement");
+  }
 }
 
 function interpIfStatement(state: State, stmt: IfStatement) {
+  const condVal = interpExpression(state, stmt.test);
+  if (typeof condVal !== "boolean") {
+    throw new Error("If condition must be a boolean");
+  }
+  const inner = new State(state);
+  if (condVal) {
+    interpBlock(inner, stmt.truePart);
+  } else interpBlock(inner, stmt.falsePart);
 }
 
 function interpWhileStatement(state: State, stmt: WhileStatement) {
+  let conditionValue = interpExpression(state, stmt.test);
+  if (typeof conditionValue !== "boolean") throw new Error("While condition must be a boolean");
+
+  while (conditionValue) {
+    const inner = new State(state);
+    interpBlock(inner, stmt.body);
+    conditionValue = interpExpression(state, stmt.test);
+    if (typeof conditionValue !== "boolean") throw new Error("While condition must be a boolean");
+  }
 }
 
-// A "block" is a list of statements. 
+// A "block" is a list of statements.
 // Consider:
 // 1. How this differs from interpProgram, which also takes in a list of statements
 // 2. How you can use this function to reduce duplication in other statement implementations.
 function interpBlock(state: State, statements: Statement[]) {
+  for (const statement of statements) {
+    interpStatement(state, statement);
+  }
 }
 
 export function interpExpression(state: State, expr: Expression): RuntimeValue {
+  switch (expr.kind) {
+    case "number":
+      return expr.value;
+    case "boolean":
+      return expr.value;
+    case "variable":
+      return state.get(expr.name);
+    case "operator":
+      return interpBinaryOperatorExpression(state, expr);
+  }
+  throw new Error("unknown expression kind");
+}
+
+function interpBinaryOperatorExpression(state: State, expr: BinaryOperatorExpression): RuntimeValue {
+  const operator = expr.operator;
+  if (operator === "&&") {
+    const left = interpExpression(state, expr.left);
+    if (typeof left !== "boolean") {
+      throw new Error("&& requires boolean operands");
+    }
+    if (!left) {
+      return false;
+    }
+    const right = interpExpression(state, expr.right);
+    if (typeof right !== "boolean") {
+      throw new Error("&& requires boolean operands");
+    }
+    return right;
+  }
+  if (operator === "||") {
+    const left = interpExpression(state, expr.left);
+    if (typeof left !== "boolean") {
+      throw new Error("|| requires boolean operands");
+    }
+    if (left) {
+      return true;
+    }
+    const right = interpExpression(state, expr.right);
+    if (typeof right !== "boolean") {
+      throw new Error("|| requires boolean operands");
+    }
+    return right;
+  }
+  const left = interpExpression(state, expr.left);
+  const right = interpExpression(state, expr.right);
+  switch (operator) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new Error(`${operator} requires number operands`);
+      }
+      if (operator === "/" && right === 0) {
+        throw new Error("can't divide by zero");
+      }
+      return operator === "+"
+        ? left + right
+        : operator === "-"
+        ? left - right
+        : operator === "*"
+        ? left * right
+        : left / right;
+    case "<":
+    case ">":
+      if (typeof left !== "number" || typeof right !== "number") {
+        throw new Error(`${operator} requires number operands`);
+      }
+      return operator === "<" ? left < right : left > right;
+    case "===":
+      return left === right;
+    default:
+      throw new Error("unknown operator");
+  }
 }
 
 // Add helper functions for binary operations here.
@@ -35,19 +155,24 @@ export function interpExpression(state: State, expr: Expression): RuntimeValue {
 //   With these, you must consider the nuanced line between when something
 //   is truly duplicated code and when it just happens to have similarities.
 
-
 export class State {
   // Use this Map to store variable bindings.
   private vars = new Map<string, RuntimeValue>();
+  private parent: State | undefined;
   // Add any other necessary private member variables here
 
   constructor(parent: State | undefined = undefined) {
+    this.parent = parent;
   }
 
   // Declares a variable with the given name and binds
   // the given value to it in the local scope.
   // Throws an error if the name already exists in the innermost scope.
   declare(name: string, value: RuntimeValue) {
+    if (this.vars.has(name)) {
+      throw new Error("variable " + name + " already declared.");
+    }
+    this.vars.set(name, value);
   }
 
   // Returns the value bound to the given name in the current environment.
@@ -56,6 +181,14 @@ export class State {
   // sequentially until the name is found.
   // Throws an error if the name cannot be found.
   get(name: string): RuntimeValue {
+    const value = this.vars.get(name);
+    if (value !== undefined) {
+      return value;
+    }
+    if (this.parent !== undefined) {
+      return this.parent.get(name);
+    }
+    throw new Error("variable " + name + " not declared.");
   }
 
   // Updates the value bound to the given name in the current environment.
@@ -63,10 +196,19 @@ export class State {
   // sequentially until the name is found.
   // Throws an error if the name cannot be found.
   set(name: string, value: RuntimeValue) {
+    if (this.vars.has(name)) {
+      this.vars.set(name, value);
+      return;
+    }
+    if (this.parent !== undefined) {
+      this.parent.set(name, value);
+      return;
+    }
+    throw new Error("variable " + name + " not declared.");
   }
 
   // DO NOT MODIFY.
-  // This is present to help us test your code. 
+  // This is present to help us test your code.
   // If you remove it, this may cause tests to fail on the autograder.
   // Returns an object containing the variable bindings of the innermost scope of this State.
   asObject() {
